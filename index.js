8fcfa31bd624ffb04177702f7e81e26be80d2657
tronexis <tronexis>
2021-09-17 00:54:19 +0600

    added env port

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ import Koa from "koa";
 import cors from "@koa/cors";
 import serve from "koa-static";
 import router from "./router";
+import { config } from "dotenv";
 
+config();
 const app = new Koa();
+const port = process.env.PORT || 4000;
 
 app.use(cors()).use(serve("./images")).use(router.routes());
 
-app.listen(4000) && console.log("Server listening on 4000");
+app.listen(port) && console.log(`Server listening on ${port}`);
